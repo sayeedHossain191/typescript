46852468d9bae514a0c5d5a0b419c62e970effe5
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-08-29 12:02:41 +0600

    first commit

---
+++ output/index.js
@@ -0,0 +1,19 @@
+"use strict";
+const course = 'Next Level Web Dev with TS';
+console.log(course);
+let playerName = 'Sayeed';
+//playerName = 32
+console.log(playerName);
+let fruits = ['apple', 'pineApple'];
+let mixed = [];
+mixed.push('sayeed', 19);
+let a;
+let b = [];
+let c;
+let d;
+a = 'hossain';
+b.push(2, 'sagor');
+let myFunc;
+myFunc = () => {
+    console.log('hello');
+};
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+const course: string = 'Next Level Web Dev with TS'
+
+console.log(course)
+
+let playerName = 'Sayeed'
+
+//playerName = 32
+
+console.log(playerName)
+
+let fruits = ['apple', 'pineApple']
+
+let mixed = []
+
+mixed.push('sayeed', 19)
+
+let a: string
+let b: (number | string)[] = []
+let c: object
+let d: {
+    name: string,
+    age: number,
+    adult: boolean
+}
+
+a = 'hossain'
+b.push(2, 'sagor')
+
+
+let myFunc: Function
+
+myFunc = () => {
+    console.log('hello')
+}
